7be35c62932feaea3a0ae2cbb074e3d061fd247a
rodrigocvg <rod300603@gmail.com>
2022-06-14 09:54:16 -0300

    lista simplesmente encadeada

---
+++ LAB03 - Data Structures/list/No.java
@@ -0,0 +1,33 @@
+public class No {
+	private int elemento; 
+	private No proximo;
+	
+	
+	public No(int elemento, No proximo) {
+		this.elemento = elemento;
+		this.proximo = proximo;
+	}
+
+	public No(int elemento) {
+		this.elemento = elemento;
+	}
+
+	public int getElemento() {
+		return elemento;
+	}
+
+	public void setElemento(int elemento) {
+		this.elemento = elemento;
+	}
+
+	public No getProximo() {
+		return proximo;
+	}
+
+	public void setProximo(No proximo) {
+		this.proximo = proximo;
+	}
+
+	
+
+}
