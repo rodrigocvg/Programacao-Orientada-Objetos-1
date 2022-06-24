16d4af67dea149aa2e91b00599e7ff6858c5b229
rodrigocvg <rod300603@gmail.com>
2022-06-23 21:29:18 -0300

    ex2 lab4

--- LAB04 - Inheritance/ex02/src/Empresa.java
+++ LAB04 - Inheritance/ex02/src/Empresa.java
@@ -1,5 +1,10 @@
 public class Empresa {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Chefe c[] = new Chefe[30];
+        Vendedor v[] = new Vendedor[30];
+        Operario o[] = new Operario[30];
+        Horista h[] = new Horista[30];
+
+        c[0] = new Chefe("Lucas", "30/06/2003");
     }
 }
